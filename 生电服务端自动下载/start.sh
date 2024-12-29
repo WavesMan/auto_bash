010a919b9205179d9c6a419a4a219cee8e39e174
Waves_Man <104572398+WavesMan@users.noreply.github.com>
2024-12-29 17:04:52 +0800

    Update start.sh

--- 生电服务端自动下载/start.sh
+++ 生电服务端自动下载/start.sh
@@ -23,13 +23,13 @@ server_version=$(echo "${server_version}" | sed 's/[^a-zA-Z0-9.-]//g')
 
 # 下载对应的脚本
 echo "下载 ${server_version} 脚本..."
-curl -LO "https://github.com/WavesMan/auto_bash/生电服务端自动下载/downloads/${server_version}"
+curl -LO "https://github.com/WavesMan/auto_bash/生电服务端自动下载/downloads/${server_version}.sh"
 
 # 检查下载是否成功
-if [ -f "${server_version}" ]; then
+if [ -f "${server_version}.sh" ]; then
     echo "下载成功。"
     # 设置下载的脚本为可执行
-    chmod +x "${server_version}"
+    chmod +x "${server_version}.sh"
     # 执行下载的脚本
     ./"${server_version}.sh"
 else
